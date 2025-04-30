73cfbb0ed7bbe12b237ded2039a2523b96baadc2
Shiva Aleti <aletishiva218@gmail.com>
2025-04-30 13:21:13 +0530

    updated glassdoor.py

--- glassdoor.py
+++ glassdoor.py
@@ -49,4 +49,5 @@ async def run_playwright():
         return await scrape_data(playwright)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    # app.run(debug=True)
